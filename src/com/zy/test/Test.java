7654ad124d76cd8442d77c7e87459fe97c9cf625
张耀 <zhangyao.hx@chinaccs.cn>
2020-03-29 15:22:05 +0800

    jdk源码学习首次提交

---
+++ src/com/zy/test/Test.java
@@ -0,0 +1,18 @@
+package com.zy.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author zhangyao
+ * @create 2020/3/28
+ */
+public class Test {
+
+    public static void main(String[] args){
+
+        Map<String,Object> hashMap=new HashMap();
+        hashMap.put("ll","s");
+
+    }
+}
